62051b65ddaea3ac595877e9137bc33a04149b8b
Steven Bready <stevenbready@gmail.com>
2018-02-12 16:23:12 -0700

    second commit

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
-require('dotenv').config()
+// require('dotenv').config()
 const express = require('express')
     , cors = require('cors')
 
 const app = express()
 app.use( cors() )
-
+let port = 3052
 app.use( express.static( `${__dirname}/..` ) );
 
-app.listen(process.env.SERVER_PORT, () =>console.log(`listening on port: ${process.env.SERVER_PORT}`))
+app.listen(port, () =>console.log(`listening on port: ${port}`))
